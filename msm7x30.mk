510ab13d531d049cd2b9cac659d12f7b9fdb3f60
Rudolf Tammekivi <rtammekivi@gmail.com>
2014-11-22 14:59:13 +0200

    msm7x30-common: Include Google's common media codecs

--- msm7x30.mk
+++ msm7x30.mk
@@ -38,6 +38,12 @@ PRODUCT_COPY_FILES += \
 	frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
 	frameworks/native/data/etc/android.hardware.usb.host.xml:system/etc/permissions/android.hardware.usb.host.xml
 
+# Media codecs
+PRODUCT_COPY_FILES += \
+	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
+
 # Graphics libraries
 PRODUCT_PACKAGES += \
 	libc2dcolorconvert \
