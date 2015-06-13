f2adaa303d7c8d73d5091050b4f521fe3e992aca
Rudolf Tammekivi <rtammekivi@gmail.com>
2015-06-13 12:55:33 +0300

    msm7x30-common: Remove custom recovery install script
    This is deprecated and does nothing.

--- releasetools.py
+++ releasetools.py
@@ -1,6 +1,6 @@
 #
 # Copyright (C) 2012 The Android Open-Source Project
-# Copyright (C) 2014  Rudolf Tammekivi <rtammekivi@gmail.com>
+# Copyright (C) 2015  Rudolf Tammekivi <rtammekivi@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,65 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-import sha
-import re
-import os
-
-import common
-
-# Replaces /system/etc/install-cm-recovery.sh with our variant.
-def ReplaceRecoveryInstall(info):
-	"""Generate a binary patch that creates the recovery image starting
-	with the boot image.  (Most of the space in these images is just the
-	kernel, which is identical for the two, so the resulting patch
-	should be efficient.)  Add it to the output zip, along with a shell
-	script that is run from init.rc on first boot to actually do the
-	patching and install the new recovery image.
-
-	recovery_img and boot_img should be File objects for the
-	corresponding images.  info should be the dictionary returned by
-	common.LoadInfoDict() on the input target_files.
-
-	Returns an Item for the shell script, which must be made
-	executable.
-	"""
-
-	diff_program = ["imgdiff"]
-	path = os.path.join(info.input_tmp, "SYSTEM", "etc", "recovery-resource.dat")
-	if os.path.exists(path):
-		diff_program.append("-b")
-		diff_program.append(path)
-		bonus_args = "-b /system/etc/recovery-resource.dat"
-	else:
-		bonus_args = ""
-
-	boot_img = common.GetBootableImage("boot.img", "boot.img",
-																		 info.input_tmp, "BOOT")
-	recovery_img = common.GetBootableImage("recovery.img", "recovery.img",
-																		 info.input_tmp, "RECOVERY")
-
-	boot_device = "/cust/image/boot.img"
-	recovery_device = "/cust/image/recovery.img"
-
-	sh = """#!/system/bin/sh
-if ! applypatch -c %(recovery_device)s %(recovery_sha1)s; then
-	log -t recovery "Installing new recovery image"
-	mount -o rw,remount /cust
-	applypatch %(bonus_args)s %(boot_device)s %(recovery_device)s %(recovery_sha1)s %(recovery_size)d %(boot_sha1)s:/system/recovery-from-boot.p
-	mount -o ro,remount /cust
-else
-	log -t recovery "Recovery image already installed"
-fi
-""" % { 'boot_size': boot_img.size,
-				'boot_sha1': boot_img.sha1,
-				'recovery_size': recovery_img.size,
-				'recovery_sha1': recovery_img.sha1,
-				'boot_device': boot_device,
-				'recovery_device': recovery_device,
-				'bonus_args': bonus_args,
-				}
-	common.ZipWriteStr(info.output_zip, "recovery/etc/install-cm-recovery.sh", sh)
 
 # Function used to write the boot/recovery image to the corresponding location.
 def InstallImage(info, mount_point, location, file):
@@ -94,4 +35,3 @@ def FullOTA_InstallEnd(info):
 		if "package_extract_file(\"boot.img\"" in cmd:
 			edify.script.remove(cmd)
 	InstallCustImage(info, "boot.img")
-	ReplaceRecoveryInstall(info)
