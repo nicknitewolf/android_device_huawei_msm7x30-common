62af5834fdfe3867ffe1fe8237c91f52cc84b30d
Steve Kondik <steve@cyngn.com>
2015-06-18 18:16:58 -0700

    msm7x30-common: Don't need to copy FFMPEG config anymore
    Change-Id: Ib5ebdc56df0cc5640161efe67e5969192e851761

--- msm7x30.mk
+++ msm7x30.mk
@@ -44,8 +44,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
 	frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
 	frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-	frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
+	frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Graphics libraries
 PRODUCT_PACKAGES += \
