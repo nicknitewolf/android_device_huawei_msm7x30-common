ae6242c89d5385ad941b63589e6e448911e307f1
niks255 <niks255292@gmail.com>
2017-01-16 16:41:40 +0300

    Reduce background apps limit to 8
    This is more than enough for these old devices.
    
    Change-Id: I9c38610241c207f33123853c132a1eb3906e19ea

--- msm7x30.mk
+++ msm7x30.mk
@@ -119,7 +119,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.hwro=1 \
 	persist.sys.strictmode.visual=0 \
 	persist.sys.strictmode.disable=1 \
-	ro.sys.fw.bg_apps_limit=16 \
+	ro.sys.fw.bg_apps_limit=8 \
 	ro.config.max_starting_bg=8 \
 	config.disable_atlas=true
 
